1f995312949f7133cf7d54ffa799b08fae9e8b7a
Quang P. Nguyen <f00345k@discovery7.hpcc.dartmouth.edu>
2020-01-20 14:22:30 -0500

    Fixing correlation rule

--- snakefiles/correlation.rule.py
+++ snakefiles/correlation.rule.py
@@ -3,23 +3,23 @@ MET = ["tar", "untar"]
 
 rule all:
     input:
-        spearman_correlation = expand("output/analysis/correlation/{time}_{met}_spearman.rds", time = TIME, met = MET),
-        sparse_cca = expand("output/analysis/correlation/{time}_{met}_scca.rds", time = TIME, met = MET)
+        spearman_correlation = expand("output/analyses/correlation/{time}_{met}_spearman.rds", time = TIME, met = MET),
+        sparse_cca = expand("output/analyses/correlation/{time}_{met}_scca.rds", time = TIME, met = MET)
 
 rule sparse_cca:
     input: 
         data = "/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/data/processed_{time}_{met}_prediction_phyloseq_obj.rds",
         script = "R/sparse_cca.R"
     output: 
-        out_file = "output/analysis/correlation/{time}_{met}_scca.rds"
+        out_file = "output/analyses/correlation/{time}_{met}_scca.rds"
     shell:
-        "RScript {input.script} --input {input.data} --output {output.out_file} --n_boot 5000 --n_perm 1000"
+        "Rscript {input.script} --input {input.data} --output {output.out_file} --n_boot 5000 --n_perm 1000"
 
 rule spearman_correlation:
     input:
         data = "/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/data/processed_{time}_{met}_prediction_phyloseq_obj.rds",
         script = "R/spearman_corr.R"
     output: 
-        out_file = "output/analysis/correlation/{time}_{met}_spearman.rds"
+        out_file = "output/analyses/correlation/{time}_{met}_spearman.rds"
     shell:
-        "RScript {input.script} --input {input.data} --output {output.out_file} --metric spearman --MHC BH"
+        "Rscript {input.script} --input {input.data} --output {output.out_file} --metric spearman --MHC BH"

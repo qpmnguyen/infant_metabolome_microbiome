078aa711cf27b1c9bb0aab21264588e98fb1f008
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2020-02-10 18:53:36 -0500

    fixing directories

--- R/bayesian_evaluation.R
+++ R/bayesian_evaluation.R
@@ -9,6 +9,7 @@ get_bayesian <- function(eval, time, type = "tar"){
   met_names <- names(summary_list)
   output <- list()
   for (i in 1:length(met_names)){
+    print(paste0("Current at ", met_names[[i]]))
     sum <- summary_list[[met_names[i]]]
     print(head(sum))
     sample_obj <- sum %>% select(id, id2, starts_with(time))
@@ -33,11 +34,11 @@ for (i in 1:length(eval)){
   for (j in 1:length(timepoints)){
     print(paste("Eval", eval[i]))
     print(paste("Time", timepoints[j]))
-    if (file.exists(glue("output/analyses/prediction/processed/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i])) == FALSE){
+    if (file.exists(glue("/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i])) == FALSE){
       mods <- get_bayesian(eval = eval[i], time = timepoints[j], type = "tar")
-      saveRDS(tidy(mods), glue("output/analyses/prediction/processed/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i]))
+      saveRDS(mods, glue("/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i]))
     } else {
-      mods <- readRDS(file = glue("output/analyses/prediction/processed/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i]))
+      mods <- readRDS(file = glue("/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/{time}_tar_{eval}_bayes.rds", time = timepoints[j], eval = eval[i]))
     }
     summaries <- lapply(mods, function(x){
       sample <- tidy(x)

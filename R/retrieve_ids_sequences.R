e956117bf5b279468da9365bc93aa875a76266d2
Quang Nguyen <quangpmnguyen@gmail.com>
2021-02-09 16:04:06 -0500

    Regenerate files

--- R/retrieve_ids_sequences.R
+++ R/retrieve_ids_sequences.R
@@ -54,36 +54,36 @@ get_index <- function(physeq){
 
 
 # Filter samples and write to biom file
-seq_6w <- readRDS(file = paste0(source_dir, "AH_Jan2020/sv6W_ST/seqtab.nochim.colnames_6W_ST.rds"))
-# Get index identifies samples with low presence in less than 10% of samples
-idx <- get_index(samples_6w)
-ntaxa(samples_6w) - length(idx)
+seq_6w <- readRDS(file = paste0(source_dir, "AH_Jan2020/sv6W_ST/seqtab.nochim.colnames_6W_ST.rds")) %>%
+  as.list()
+# Get sequence names
 names(seq_6w) <- paste0("SV",1:length(seq_6w))
-seq_6w <- seq_6w %>% as.list()
-seq_6w <- seq_6w[-idx]
-write.fasta(seq_6w, names = names(seq_6w), file.out = "output/6w_asv_filt.fa")
-
-# save samples_6w as biom files 
+# Filter out ASVs with at least presence in 10% of samples
 samples_6w <- filter_taxa(samples_6w, function(x) sum(x > 0) >= 0.1*length(x), TRUE)
+
+idx <- which(names(seq_6w) %in% taxa_names(samples_6w))
+seq_6w <- seq_6w[idx]
+
+write.fasta(seq_6w, names = names(seq_6w), file.out = "picrust2/seqs/6w_asv_filt.fa")
+
 biom_6w <- make_biom(
   data = otu_table(samples_6w, taxa_are_rows = FALSE)
 )
-write_biom(biom_6w, biom_file = "output/biom_6w.biom")
+write_biom(biom_6w, biom_file = "picrust2/bioms/biom_6w.biom")
 
 # Get sequences for 12m 
 seq_12m <- readRDS(file = paste0(source_dir, "AH_Jan2020/sv12M_ST/seqtab.nochim.colnames_12M_ST.rds"))
-idx <- get_index(samples_12m)
-ntaxa(samples_12m) - length(idx)
-
 names(seq_12m) <- paste0("SV", 1:length(seq_12m))
-seq_12m <- seq_12m[-idx]
-write.fasta(as.list(seq_12m), names = names(seq_12m), file.out = "output/12m_asv_filt.fa")
-
 samples_12m <- filter_taxa(samples_12m, function(x) sum(x > 0) >= 0.1 * length(x), TRUE)
+idx <- which(names(seq_12m) %in% taxa_names(samples_12m))
+seq_12m <- seq_12m[idx]
+write.fasta(as.list(seq_12m), names = names(seq_12m), file.out = "picrust2/seqs/12m_asv_filt.fa")
+
+
 biom_12m <- make_biom(
   data = otu_table(samples_12m, taxa_are_rows = FALSE)
 )
-write_biom(biom_12m, biom_file = "output/biom_12m.biom")
+write_biom(biom_12m, biom_file = "picrust2/bioms/biom_12m.biom")
 
 
 # Section on getting quality scores post filtering 
